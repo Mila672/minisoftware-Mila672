dddce2adc40090ec877f1e20a65d010fce595dcf
Michelle Lahmer <mlahmer@wbsedu.de>
2026-08-20 16:29:40 +0200

    Capacitor ohne TypeScript-Konfiguration ausfuehren, damit der Android-Build im GitHub-Runner nicht abbricht

---
+++ capacitor.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@capacitor/cli').CapacitorConfig} */
+const config = {
+  appId: 'com.mila672.minisoftware',
+  appName: 'Mila672',
+  webDir: 'web',
+  bundledWebRuntime: false
+};
+
+module.exports = config;
